c175c8f8f05dd7c896d9aa2423680369b7194ff2
Murilo Amendola <muriloamendola@gmail.com>
2010-07-31 16:21:59 -0300

    mecanismo de selecao multipla checkboxes jquery implementado

--- public/javascripts/easyclinica.js
+++ public/javascripts/easyclinica.js
@@ -1,10 +1,12 @@
 //processada assim que a renderização da página é concluída
 $(document).ready(function(){
-		setup();
+		setup(document);
 });
 
-function setup(){
-	criar_mascaras_campos();
+function setup(main_selector){
+	criar_mascaras_campos(main_selector);
+	
+	check_all_checkboxes(main_selector);
 }
 
 /*
@@ -13,7 +15,24 @@ function setup(){
 	mask_telefone: (99) 9999-9999
 	mask_cep:			 99999-999
 */
-function criar_mascaras_campos(){
-	$('.mask_telefone').mask('(99) 9999-9999');
-	$('.mask_cep').mask('99999-999');
+function criar_mascaras_campos(main_selector){
+	$(main_selector).find('.mask_telefone').mask('(99) 9999-9999');
+	$(main_selector).find('.mask_cep').mask('99999-999');
+}
+
+/*
+	mecanismo de seleção multipla de checkbox. Exemplo:
+	defina para o checkbox que ativará a seleção multipla a classe check_all e na propriedade rel defina
+	o rel dos checkboxes que serão controlados.
+*/
+function check_all_checkboxes(main_selector) {
+	$(main_selector).find('.check_all').change(function() {
+		var pai = $(this);
+		var rel = $(pai).attr('rel');
+		
+		$(main_selector).find("input[rel='" + rel + "']").each(function(index){
+				$(this).attr('checked', $(pai).attr('checked'));
+		});	
+		
+	});
 }
