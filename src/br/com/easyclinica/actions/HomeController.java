f4f8abcde643c6705887a8b797e4346924c82adf
Mauricio Aniche <mauricioaniche@gmail.com>
2010-08-17 23:39:41 -0300

    fake dashboard

---
+++ src/br/com/easyclinica/actions/HomeController.java
@@ -0,0 +1,13 @@
+package br.com.easyclinica.actions;
+
+import br.com.caelum.vraptor.Get;
+import br.com.caelum.vraptor.Path;
+import br.com.caelum.vraptor.Resource;
+
+@Resource
+public class HomeController {
+
+	@Get
+	@Path("/")
+	public void dashboard(){}
+}
