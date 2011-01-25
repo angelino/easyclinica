c3893a340f66767964c60969a3c2fc98f7f29d58
Mauricio Aniche <mauricioaniche@gmail.com>
2011-01-25 13:26:21 -0200

    view model para receber os dados de uma nova consulta

---
+++ src/br/com/easyclinica/view/model/AppointmentData.java
@@ -0,0 +1,67 @@
+package br.com.easyclinica.view.model;
+
+import java.util.Calendar;
+import java.util.List;
+
+public class AppointmentData {
+
+	private Calendar date;
+	private int doctor;
+	private int healthCarePlan;
+	private int specialty;
+	private boolean isReturn;
+	
+	private List<ProcedureData> procedures;
+
+	public Calendar getDate() {
+		return date;
+	}
+
+	public void setDate(Calendar date) {
+		this.date = date;
+	}
+
+	public int getDoctor() {
+		return doctor;
+	}
+
+	public void setDoctor(int doctor) {
+		this.doctor = doctor;
+	}
+
+	public int getHealthCarePlan() {
+		return healthCarePlan;
+	}
+
+	public void setHealthCarePlan(int healthCarePlan) {
+		this.healthCarePlan = healthCarePlan;
+	}
+
+	public int getSpecialty() {
+		return specialty;
+	}
+
+	public void setSpecialty(int specialty) {
+		this.specialty = specialty;
+	}
+
+	public boolean isReturn() {
+		return isReturn;
+	}
+
+	public void setReturn(boolean isReturn) {
+		this.isReturn = isReturn;
+	}
+
+	public List<ProcedureData> getProcedures() {
+		return procedures;
+	}
+
+	public void setProcedures(List<ProcedureData> procedures) {
+		this.procedures = procedures;
+	}
+	
+	
+	
+	
+}
---
+++ src/br/com/easyclinica/view/model/ProcedureData.java
@@ -0,0 +1,31 @@
+package br.com.easyclinica.view.model;
+
+import java.util.List;
+
+public class ProcedureData {
+
+	private int id;
+	private List<MaterialData> materials;
+	private List<MedicineData> medicines;
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public List<MaterialData> getMaterials() {
+		return materials;
+	}
+	public void setMaterials(List<MaterialData> materials) {
+		this.materials = materials;
+	}
+	public List<MedicineData> getMedicines() {
+		return medicines;
+	}
+	public void setMedicines(List<MedicineData> medicines) {
+		this.medicines = medicines;
+	}
+	
+	
+}
