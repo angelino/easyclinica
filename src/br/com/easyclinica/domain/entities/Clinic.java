aac76d0aa71bf8ee4a49744e0c7a00a168c0fbc5
Mauricio Aniche <mauricioaniche@gmail.com>
2011-02-01 10:41:56 -0200

    clinica com fk para convenio particular

--- src/br/com/easyclinica/domain/entities/Clinic.java
+++ src/br/com/easyclinica/domain/entities/Clinic.java
@@ -3,6 +3,7 @@ package br.com.easyclinica.domain.entities;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.persistence.ManyToOne;
 
 @Entity
 public class Clinic {
@@ -13,6 +14,8 @@ public class Clinic {
 	private String name;
 	private String domain;
 	private boolean active;
+	@ManyToOne
+	private HealthCarePlan privatePlan;
 	
 	public Clinic() {
 		
@@ -49,8 +52,14 @@ public class Clinic {
 	protected void setActive(boolean active) {
 		this.active = active;
 	}
-	
 
-	
+	public HealthCarePlan getPrivatePlan() {
+		return privatePlan;
+	}
+
+	public void setPrivatePlan(HealthCarePlan privatePlan) {
+		this.privatePlan = privatePlan;
+	}
+
 	
 }
