069a7bcd5188f31fba5e3ac8a4445774da45071a
Mauricio Aniche <mauricioaniche@gmail.com>
2011-02-21 15:02:53 -0300

    campos necessarios no cadastro de clinica

--- src/br/com/easyclinica/domain/entities/Clinic.java
+++ src/br/com/easyclinica/domain/entities/Clinic.java
@@ -1,10 +1,13 @@
 package br.com.easyclinica.domain.entities;
 
+import javax.persistence.Embedded;
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.ManyToOne;
+
+import br.com.easyclinica.domain.types.Address;
 
 @Entity
 public class Clinic {
@@ -15,7 +18,14 @@ public class Clinic {
 	private String name;
 	private String domain;
 	private boolean active;
-	
+	@Embedded private Address address;
+	private String billingEmail;
+	private String contactEmail;
+	private String telephone;
+	private String telephone2;
+	private String fax;
+	private String website;
+	private String cnpj;
 	@ManyToOne(fetch=FetchType.EAGER)
 	private HealthCarePlan privatePlan;
 	
@@ -59,6 +69,70 @@ public class Clinic {
 
 	public void setPrivatePlan(HealthCarePlan privatePlan) {
 		this.privatePlan = privatePlan;
+	}
+
+	public Address getAddress() {
+		return address;
+	}
+
+	public void setAddress(Address address) {
+		this.address = address;
+	}
+
+	public String getBillingEmail() {
+		return billingEmail;
+	}
+
+	public void setBillingEmail(String billingEmail) {
+		this.billingEmail = billingEmail;
+	}
+
+	public String getContactEmail() {
+		return contactEmail;
+	}
+
+	public void setContactEmail(String contactEmail) {
+		this.contactEmail = contactEmail;
+	}
+
+	public String getTelephone() {
+		return telephone;
+	}
+
+	public void setTelephone(String telephone) {
+		this.telephone = telephone;
+	}
+
+	public String getTelephone2() {
+		return telephone2;
+	}
+
+	public void setTelephone2(String telephone2) {
+		this.telephone2 = telephone2;
+	}
+
+	public String getFax() {
+		return fax;
+	}
+
+	public void setFax(String fax) {
+		this.fax = fax;
+	}
+
+	public String getWebsite() {
+		return website;
+	}
+
+	public void setWebsite(String website) {
+		this.website = website;
+	}
+
+	public String getCnpj() {
+		return cnpj;
+	}
+
+	public void setCnpj(String cnpj) {
+		this.cnpj = cnpj;
 	}
 
 	
