0da5700be66d14d077865c896c095377741d3d7d
Murilo Amendola <muriloamendola@gmail.com>
2011-05-14 11:25:08 -0300

    enter na busca de pacientes funcionando

--- WebContent/javascripts/pacientes.js
+++ WebContent/javascripts/pacientes.js
@@ -5,30 +5,36 @@ EasyClinica.pages['pacientes'] = function(){
 		
 		searchPatients(1);
 	});
-};
-
-var searchPatients = function(page) {
-	$(this).after("<img id='loading' src='" + EasyClinica.cfg.images.loading + "' alt='carregando...'/>");
 	
-	var url = EasyClinica.cfg.services.searchPatients;
-	var texto = $('input[name=patient.textobusca]').val();
-	
-	$.post(url, {text: texto, page: page}, function(data){
-		$('#box_listagem').html(data);
-		EasyClinica.common.generalFunctions();
-		//showConfiguration();		
-		managerPagination();
+	var searchPatients = function(page) {
+		$(this).after("<img id='loading' src='" + EasyClinica.cfg.images.loading + "' alt='carregando...'/>");
 		
-		$('#loading').remove();
+		var url = EasyClinica.cfg.services.searchPatients;
+		var texto = $('input[name=patient.textobusca]').val();
+		
+		$.post(url, {text: texto, page: page}, function(data){
+			$('#box_listagem').html(data);
+			EasyClinica.common.generalFunctions();
+			//showConfiguration();		
+			managerPagination();
+			
+			$('#loading').remove();
+		});
+	};
+
+	var managerPagination = function() {
+		$('.boxpagination a[page]').click(function(e){
+			e.preventDefault();
+			
+			var page = $(this).attr("page");
+			searchPatients(page);
+		});
+	};
+	managerPagination();
+	
+	$('input[name=patient.textobusca]').keydown(function(event){
+		if(event.keyCode == 13) {
+			searchPatients(1);
+	    }
 	});
 };
-
-var managerPagination = function() {
-	$('.boxpagination a[page]').click(function(e){
-		e.preventDefault();
-		
-		var page = $(this).attr("page");
-		searchPatients(page);
-	});
-};
-
