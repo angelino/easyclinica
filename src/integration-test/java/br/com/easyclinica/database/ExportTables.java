ee912156162e37b4a15d46d61293a18c362ce682
Murilo Amendola <muriloamendola@gmail.com>
2011-05-31 20:29:12 -0300

    ajustes agenda

--- src/integration-test/java/br/com/easyclinica/database/ExportTables.java
+++ src/integration-test/java/br/com/easyclinica/database/ExportTables.java
@@ -20,9 +20,9 @@ public class ExportTables {
 		se.create(true, true);
 	}
 	
-	@Test @Ignore
+	@Test
 	public void updateSchema() {
-		Configuration cfg = new DatabaseConfigurator(new HardCodedConfigForTests()).config("easyclinicatest");
+		Configuration cfg = new DatabaseConfigurator(new HardCodedConfigForTests()).config("easyclinica");
 		
 		SchemaUpdate se = new SchemaUpdate(cfg);
 		se.execute(true, true);
